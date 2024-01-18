a26f0e6025209c2843220fb153315f063ede3e7d
Korolev E <nuuchcha@bk.ru>
2024-01-18 20:14:58 +0500

    Modified WebTest.java

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -1,36 +1,11 @@
 import com.codeborne.selenide.SelenideElement;
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class WebTest {
-//    WebDriver driver;
-//
-//    @BeforeAll
-//    static void setupAll() {
-//        WebDriverManager.chromedriver().setup();
-//    }
-//
-//    @BeforeEach
-//    void setUp() {
-//        driver = new ChromeDriver();
-//    }
-//
-//    @AfterEach
-//    void tearDown(){
-//        driver.quit();
-//        driver = null;
-//    }
 
     @Test
     void applicationMustPass() throws InterruptedException {
@@ -41,8 +16,6 @@ class WebTest {
         form.$("[data-test-id=phone] input").setValue("+79876543210");
         form.$("[data-test-id=agreement]").click();
         form.$(".button").click();
-        $(".order-success");
-
-        Thread.sleep(5000);
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 }
